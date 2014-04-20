bcd5e4952853d2cb35fe2537e42e72740f0c6f5a
Nathan Grey <greynng@gmail.com>
2014-04-20 12:43:01 -0700

    add post functionality

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			flash[:notice] = 'New Post created.'
+			redirect_to posts_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+		flash[:notice] = 'Post updated.'
+		if @post.update(post_params)
+			redirect_to post_path
+		else
+			render 'new'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		flash[:notice] = 'Post deleted.'
+		redirect_to posts_path
+	end
+
+private
+
+	def post_params
+		params.require(:post).permit(:author, :title, :content)
+	end
+end
